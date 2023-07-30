f1c260063650be9c240590b8d43998b149e85008
hamid <ahmidatabdelhamid@gmail.com>
2023-07-30 03:46:05 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const name = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(array, eventName) {
+    const thankYouCard = [];
+    for (let i = 0; i < array.length; i++) {
+        thankYouCard.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankYouCard;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "birthday");
+
+function countDown(number) {
+
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
